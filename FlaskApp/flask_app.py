264f30aa8a8bf061bf7529b62a75c9e20329f24c
Dhanush Suryavamshi <suryavamshi221b@gmail.com>
2021-09-23 17:28:47 +0530

    Adding subprocess

--- FlaskApp/flask_app.py
+++ FlaskApp/flask_app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
+import subprocess
 import json
 
 app = Flask(__name__)
-
+FILENAME = "flask_app.py"
 @app.route('/webhook',methods=['POST'])
 def webhook():
    data = json.loads(request.data)
    print("New commit by: {}".format(data['commits'][0]['author']['name']))
+   if data["added"]:
+      if FILENAME in data["added"]:
+         resp = subprocess.Popen(["echo flask file updated"], stdout=subprocess.PIPE)
+         output = resp.communicate()[0]
+         print(output.decode("utf-8"))
+
    return "OK"
 
 if __name__ == '__main__':
