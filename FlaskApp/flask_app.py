1f3d74865ca1187f23dca6c6218161645fbd332f
Dhanush Suryavamshi <suryavamshi221b@gmail.com>
2021-09-23 18:13:57 +0530

    Adding more logic

--- FlaskApp/flask_app.py
+++ FlaskApp/flask_app.py
@@ -15,9 +15,11 @@ def webhook():
             resp = subprocess.Popen(["echo ", '"flask file updated"'], shell=True, stdout=subprocess.PIPE)
             output = resp.communicate()[0]
             print(output.decode("utf-8"))
+            print("working")
       elif data["commits"][0]["modified"]:
          if FILENAME in data["commits"][0]["modified"]:
             resp = subprocess.Popen(["echo ", '"flask file updated"'], shell=True, stdout=subprocess.PIPE)
+            print("working")
             output = resp.communicate()[0]
             print(output.decode("utf-8"))
    return "OK"
